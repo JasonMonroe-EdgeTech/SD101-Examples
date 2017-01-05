20d509ca2fa0c4ecbbd81220b7fa4472ccb5811c
Jason Monroe <jmonroe@ancoraeducation.com>
2017-01-05 10:48:48 -0600

    Updates

--- scripts/JS-Day-2-For-Loop.js
+++ scripts/JS-Day-2-For-Loop.js
@@ -4,3 +4,13 @@ function displayMultiples(){
      spnResult.innerHTML += n +" ";
   }
 }
+
+
+function displayFloors(){
+  var spnResult = document.getElementById('spnResult');
+  for(n=1;n<18;n+=1){
+    if(n==13)
+      continue;
+    spnResult.innerHTML += n +" ";
+  }
+}
