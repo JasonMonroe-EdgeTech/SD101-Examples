0fe8092abb8aba4f9a6ca5fe544305bf486d1437
Jason Monroe <jmonroe@ancoraeducation.com>
2017-01-25 16:23:56 -0600

    Dali

--- CSS-Dali/scripts/formValidation.js
+++ CSS-Dali/scripts/formValidation.js
@@ -3,8 +3,8 @@ window.onload = function() {
         return validateForm();
     }
 
-    document.getElementById('txtCardNumber').addEventListener('keyup', ccInputMask);
-    document.getElementById('txtPhoneNumber').addEventListener('keyup', phoneInputMask);
+    document.getElementById('txtCardNumber').onkeyup =  ccInputMask;
+    document.getElementById('txtPhoneNumber').onkeyup = phoneInputMask;
 
 }
 
@@ -58,45 +58,35 @@ function ccInputMask(event) {
 
 }
 
-function phoneInputMask() {
+function phoneInputMask(event) {
     var phoneNumberTextbox = document.getElementById('txtPhoneNumber');
-    var unmaskedPhoneNum = cardNumberText.value.replace(/-/g, "").replace(/\(/g, "").replace(/\)/g, "");
+    var unmaskedPhoneNum = phoneNumberTextbox.value.replace(/-/g, "").replace(/\(/g, "").replace(/\)/g, "").replace(/ /g,"");
 
 
     if(event.keyCode == 8){
       return;
     }
 
-    switch (unmaskedCardNum.length) {
+    switch (unmaskedPhoneNum.length) {
         case 0:
         case 1:
         case 2:
+          break;
         case 3:
+            phoneNumberTextbox.value = "("+unmaskedPhoneNum+") ";
             break;
         case 4:
-            cardNumberText.value = unmaskedPhoneNum+"-";
-            break;
         case 5:
+            break;
         case 6:
+            phoneNumberTextbox.value = "("+unmaskedPhoneNum.substr(0, 3) + ") " + unmaskedPhoneNum.substr(3, 3)+"-";
+            break;
         case 7:
-            break;
         case 8:
-            cardNumberText.value = unmaskedPhoneNum.substr(0, 4) + "-" + unmaskedPhoneNum.substr(4, 4)+"-";
-            break;
         case 9:
+          break;
         case 10:
-        case 11:
+            phoneNumberTextbox.value = "("+unmaskedPhoneNum.substr(0, 3) + ") " + unmaskedPhoneNum.substr(3, 3)+"-"+unmaskedPhoneNum.substr(6,4);
             break;
-        case 12:
-            cardNumberText.value = unmaskedPhoneNum.substr(0, 4) + "-" + unmaskedPhoneNum.substr(4, 4) + "-" + unmaskedPhoneNum.substr(8, 4)+"-";
-            break;
-        case 13:
-        case 14:
-        case 15:
-            break;
-        case 16:
-            cardNumberText.value = unmaskedPhoneNum.substr(0, 4) + "-" + unmaskedPhoneNum.substr(4, 4) + "-" + unmaskedPhoneNum.substr(8, 4)+ "-" + unmaskedPhoneNum.substr(12, 4);
-            break;
-
     }
 }
