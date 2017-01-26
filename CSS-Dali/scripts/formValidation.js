49e5f5524ede357a226668ed7d90b8bcca3d172b
Jason Monroe <jmonroe@ancoraeducation.com>
2017-01-26 16:59:13 -0600

    Custom Objects

--- CSS-Dali/scripts/formValidation.js
+++ CSS-Dali/scripts/formValidation.js
@@ -56,31 +56,14 @@ function ccInputMask(event) {
     }
 
     switch (unmaskedCardNum.length) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-            break;
         case 4:
             cardNumberTextbox.value = unmaskedCardNum+"-";
-            break;
-        case 5:
-        case 6:
-        case 7:
             break;
         case 8:
             cardNumberTextbox.value = unmaskedCardNum.substr(0, 4) + "-" + unmaskedCardNum.substr(4, 4)+"-";
             break;
-        case 9:
-        case 10:
-        case 11:
-            break;
         case 12:
             cardNumberTextbox.value = unmaskedCardNum.substr(0, 4) + "-" + unmaskedCardNum.substr(4, 4) + "-" + unmaskedCardNum.substr(8, 4)+"-";
-            break;
-        case 13:
-        case 14:
-        case 15:
             break;
         case 16:
             cardNumberTextbox.value = unmaskedCardNum.substr(0, 4) + "-" + unmaskedCardNum.substr(4, 4) + "-" + unmaskedCardNum.substr(8, 4)+ "-" + unmaskedCardNum.substr(12, 4);
@@ -100,23 +83,12 @@ function phoneInputMask(event) {
     }
 
     switch (unmaskedPhoneNum.length) {
-        case 0:
-        case 1:
-        case 2:
-          break;
         case 3:
             phoneNumberTextbox.value = "("+unmaskedPhoneNum+") ";
-            break;
-        case 4:
-        case 5:
             break;
         case 6:
             phoneNumberTextbox.value = "("+unmaskedPhoneNum.substr(0, 3) + ") " + unmaskedPhoneNum.substr(3, 3)+"-";
             break;
-        case 7:
-        case 8:
-        case 9:
-          break;
         case 10:
             phoneNumberTextbox.value = "("+unmaskedPhoneNum.substr(0, 3) + ") " + unmaskedPhoneNum.substr(3, 3)+"-"+unmaskedPhoneNum.substr(6,4);
             break;
--- Custom-Objects/scripts/main.js
+++ Custom-Objects/scripts/main.js
@@ -5,6 +5,17 @@ function artPiece(id,name, desc, price){
   this.name = name;
   this.desc = desc;
   this.price = price;
+  this.priceEuros = convertToEuros;
+  this.displayItem = function(){
+    return "<tr><td>"+this.id+"</td>"+
+           "<td>"+ this.name+"</td>"+
+           "<td>"+ this.desc+"</td>"+
+           "<td style='width:100px; text-align: right;'>"+ this.price+"</td></tr>";
+  }
+}
+
+function convertToEuros(){
+  return Math.round(this.price * 0.94,2);
 }
 
 
@@ -12,6 +23,8 @@ function computeSubtotal(){
 
     var purchases = [];
     var subtotal = 0;
+    var subtotalEuros = 0;
+    var itemsTable =   "<table><tr><th>ID</th><th>Name</th><th>Desc</th><th>Price</th></tr>";
 
     //creates new artPiece object instances and stores in purchases array
     purchases[0] = new artPiece(1002,"Persistence of Memory", "3' X 5' poster", 14.99);
@@ -20,8 +33,10 @@ function computeSubtotal(){
     //cycles through array summing price property
     for(var n = 0 ; n < purchases.length; n++){
       subtotal += purchases[n].price;
+      itemsTable += purchases[n].displayItem();
     }
 
     //display results
-    document.getElementById('spnSubtotal').innerHTML = "Subtotal: $"+subtotal;
+    document.getElementById('divItems').innerHTML = itemsTable+"</table>";
+    document.getElementById('divSubtotal').innerHTML = "Subtotal: $"+subtotal;
 }
