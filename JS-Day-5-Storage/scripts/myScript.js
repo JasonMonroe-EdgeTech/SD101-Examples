f074ca022d4c2c239a737cde454f69034933a8c3
Jason Monroe <jmonroe@ancoraeducation.com>
2017-01-31 14:22:16 -0600

    storage examples

---
+++ JS-Day-5-Storage/scripts/myScript.js
@@ -0,0 +1,14 @@
+
+
+console.log(sessionStorage.HackerName+"..."+sessionStorage.Message);
+console.log(localStorage.UserName+" High Score: "+localStorage.HighScore);
+
+sessionStorage.HackerName = "Neo";
+sessionStorage.Message = "The Matrix Has you...";
+
+localStorage.UserName = "YatzeeKing";
+localStorage.HighScore = "622";
+
+console.log("----------------------------------");
+console.log(sessionStorage.HackerName+"..."+sessionStorage.Message);
+console.log(localStorage.UserName+" High Score: "+localStorage.HighScore);
