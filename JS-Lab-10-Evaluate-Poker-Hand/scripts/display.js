8b2853d1cf13e9375eadf60caf6937225e267be4
Jason Monroe <jmonroe@ancoraeducation.com>
2017-05-10 14:56:12 -0500

    fixed JPG

--- JS-Lab-10-Evaluate-Poker-Hand/scripts/display.js
+++ JS-Lab-10-Evaluate-Poker-Hand/scripts/display.js
@@ -3,5 +3,5 @@ function changeCard(cardNum){
   var imgCard = document.getElementById("card"+cardNum);
   var rank = document.getElementById("selectRank"+cardNum).value;
   var suit = document.getElementById("selectSuit"+cardNum).value;
-  imgCard.src = "./images/cards/"+rank+suit+".jpg";
+  imgCard.src = "./images/cards/"+rank+suit+".JPG";
 }
