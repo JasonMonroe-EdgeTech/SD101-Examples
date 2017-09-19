cc2b298c9454d906cfab4109d6ea6189290356ce
JasonMonroe-EdgeTech <jmonroe@ancoraeducation.com>
2017-09-19 17:09:21 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,8 @@
+
+
+function message(){
+  var txtName = document.getElementById("txtName");
+  var divMessage = document.getElementById("divMessage");
+
+  divMessage.innerHTML = txtName.value+"..."+"<br/>The Matrix has you...";
+}
